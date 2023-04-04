caf79b19cd29730b788c4e9b4fcd7846cae35991
Vlad Nastase <vladdnastase@gmail.com>
2023-04-04 15:40:16 +0300

    Fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://vladnastase.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-bug',
+  baseUrl: '/docusaurus-bug/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
